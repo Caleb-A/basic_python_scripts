539bfe74c1cc0992ebf703f0a8490decf7a03a1b
Caleb Akinosho <cakinsoho@gmail.com>
2020-08-02 19:52:32 -0400

    first commit

---
+++ temperature_converter.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+from math import *
+#Converts Fahrenheit into Celsius and vice versa
+print("F is for Fahrenheit")
+print("C is for Celsius")
+temp = input("Choose your unit of temperature Celsius(C) or Fahrenheit(F):  ")
+
+if temp == 'F':
+    fahrenheit = float(input("What is the temperature in Fahrenheit: "))
+    int()
+    print(float((fahrenheit-32)/1.8))
+elif temp == 'C':
+    celsius = float(input("What is the temperature in Celsius: "))
+    print(float((celsius*1.8)+32))
+else:
+    print("Invalid Input")
